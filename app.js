fa2ebafe8829368129d8575039fc7be941470e5a
Julia Fong <Juliamzfong@gmail.com>
2020-09-15 20:42:31 -0500

    First commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const chalk = require('chalk')
+const yargs = require('yargs')
+
+
+//Customize yargs version
+yargs.version('1.1.0')
+
+//Create add command
+
+yargs.command({
+
+})
+
+//add, remove, read, list
+
+console.log(yargs.argv)
