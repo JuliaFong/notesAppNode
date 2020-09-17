ccbc734eb6826aacb69433151150a373a9501a1f
Julia Fong <Juliamzfong@gmail.com>
2020-09-17 12:07:29 -0500

    Add note function

---
+++ README.md
@@ -0,0 +1,10 @@
+# Notes App
+An app that allows you to add, remove, read, and list your notes
+
+# Technologies used
+Node.js
+Yargs
+
+# Learning goals
+* Review Node.js
+* Learn how to use yargs in conjunction to node.js
--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const chalk = require('chalk')
 const yargs = require('yargs')
+const notes = require('./notes.js');
 
 
 //Customize yargs version
@@ -21,8 +22,7 @@ yargs.command({
         }
     },
     handler: function(argv) {
-        console.log('Title: ' +  argv.title)
-        console.log('Body: ' + argv.body)
+      notes.addNote(argv.title, argv.body)
     }
 })
 
